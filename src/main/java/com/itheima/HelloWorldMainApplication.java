c5602a2ea7f590c51ed0e0c4b692854463d91804
yaya514 <2487261885@qq.com>
2019-04-29 19:10:45 +0800

    first commit

---
+++ src/main/java/com/itheima/HelloWorldMainApplication.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //表示这个是SpringBoot应用
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+
+        //spring应用启动起来
+        SpringApplication.run(HelloWorldMainApplication.class);
+    }
+
+}
